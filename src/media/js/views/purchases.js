fd6327ace291dd61ac0bd77b267529134b217c6a
atiqueahmedziad <softfilebd@gmail.com>
2015-12-05 05:33:05 -0500

    Fix hub/spoke header for Homepage & My Apps

--- src/media/js/views/purchases.js
+++ src/media/js/views/purchases.js
@@ -1,14 +1,16 @@
 /*
     My Apps page.
 */
-define('views/purchases', ['core/l10n', 'core/urls'], function(l10n, urls) {
+define('views/purchases', ['core/l10n', 'core/urls', 'utils_local'], function(l10n, urls, utilsLocal) {
     'use strict';
     var gettext = l10n.gettext;
+    var title = gettext('My Apps');
 
     return function(builder, args) {
         builder.z('type', 'root settings purchases');
-        builder.z('title', gettext('My Apps'));
+        builder.z('title', title);
         builder.z('parent', urls.reverse('homepage'));
+        utilsLocal.headerTitle(title);
 
         builder.start('purchases.html', {
             endpoint_name: 'installed'
